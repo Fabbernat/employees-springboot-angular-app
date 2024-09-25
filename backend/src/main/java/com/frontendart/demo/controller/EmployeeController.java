0f0158a977f490481ecbef59e252ec73a6148dc9
Fabbernat <h259147@stud.u-szeged.hu>
2024-09-25 11:00:26 +0200

    Set up environment, everything is running perfectly. No new thing is implemented yet.

--- backend/src/main/java/com/frontendart/demo/controller/EmployeeController.java
+++ backend/src/main/java/com/frontendart/demo/controller/EmployeeController.java
@@ -5,26 +5,23 @@ import com.frontendart.demo.entity.Employee;
 import com.frontendart.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
 
-  @Autowired
-  EmployeeService employeeService;
+    @Autowired
+    EmployeeService employeeService;
 
-  @PostMapping(value = "/employee")
-  public ResponseEntity<Employee> addNewEmployee(@RequestBody EmployeeDto employeeDto) {
-    return ResponseEntity.ok(employeeService.createEmployee(employeeDto));
-  }
+    @PostMapping(value = "/employee")
+    public ResponseEntity<Employee> addNewEmployee(@RequestBody EmployeeDto employeeDto) {
+        return ResponseEntity.ok(employeeService.createEmployee(employeeDto));
+    }
 
-  @GetMapping(value = "/employee/{id}")
-  public ResponseEntity<Employee> getEmployee(@PathVariable Long id){
-    return employeeService.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
-  }
+    @GetMapping(value = "/employee/{id}")
+    public ResponseEntity<Employee> getEmployee(@PathVariable Long id) {
+        return employeeService.findById(id).map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
 
 }
--- backend/src/main/java/com/frontendart/demo/repository/EmployeeRepository.java
+++ backend/src/main/java/com/frontendart/demo/repository/EmployeeRepository.java
@@ -3,8 +3,8 @@ package com.frontendart.demo.repository;
 import com.frontendart.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 
 }
