9ed2eae465a570913c0697676ad4321480745161
Fabbernat <h259147@stud.u-szeged.hu>
2024-09-25 19:09:55 +0200

    Frontend employee-listing empty list error message works properly and is added to app.component.html

--- frontend/demo-app/src/app/employee-list/employee-list.component.ts
+++ frontend/demo-app/src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService, Employee } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-list.component.scss']
 })
 export class EmployeeListComponent {
+  employees: Employee[] = [];
+  errorMessage: string = '';
 
+  constructor(private employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.loadEmployees();
+  }
+
+  loadEmployees(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (data) => {
+        // this.employees = data;
+        this.errorMessage = '';  // Clear any previous error message
+      },
+      error: (err) => {
+        this.errorMessage = 'Could not load employee data. Please try again later.';
+        console.error(err);
+      }
+    });
+  }
 }
